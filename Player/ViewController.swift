a9ce39bea6a3aae82a08ade39643d41d98a8203a
Donavin Wats <donnist@gmail.com>
2020-04-15 20:44:15 -0400

    Music Player with Swipe Gestures

--- Player/ViewController.swift
+++ Player/ViewController.swift
@@ -9,8 +9,12 @@
 import UIKit
 import AVFoundation
 
-var audioPlayer: AVAudioPlayer = AVAudioPlayer();
-var currentTime = audioPlayer.currentTime;
+var lightOn: Bool = true
+var audioPlayer: AVAudioPlayer = AVAudioPlayer() 
+var currentTime = audioPlayer.currentTime 
+let PlayerPadView = UIView()
+let nightYellow = #colorLiteral(red: 0.3098039329, green: 0.2039215714, blue: 0.03921568766, alpha: 1)
+let nightText = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
 
 
 //Creates random UIColors
@@ -25,49 +29,83 @@ class ViewController: UIViewController {
 
 	@IBOutlet weak var backgroundView: UIView!
 	
+	@IBOutlet weak var PlayerGUI: UILabel!
+	
+	@IBOutlet weak var bgView: UIView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 	
 		// Do any additional setup after loading the view.
-		let path: String = Bundle.main.path(forResource: "musette", ofType: "mp3")!;
-		print("path = \(path)");
+		
+		
+		let path: String = Bundle.main.path(forResource: "musette", ofType: "mp3")! 
+		print("path = \(path)") 
 		
 		do {
-			audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path));
+			audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path)) 
 		} catch {
 			print(error)
 		}
-		
-		audioPlayer.numberOfLoops = 0;  //Plays once (-1 for infinite loop)
-		audioPlayer.volume = 0.5;
-		audioPlayer.play();
+		audioPlayer.enableRate = true
+		audioPlayer.prepareToPlay()
+		audioPlayer.numberOfLoops = 0   //Plays once (-1 for infinite loop)
+		audioPlayer.volume = 0.5 
+		audioPlayer.play() 
 	}
-	@IBAction func SwipeRight(_ sender: UISwipeGestureRecognizer) {
-		if sender.direction == .right {
-		print("Swipped Right")
-		audioPlayer.play(atTime: currentTime + 30.0)
-		backgroundView.backgroundColor = random()
+	
+	@IBAction func swipeGesture(_ sender: UISwipeGestureRecognizer) {
+		switch sender.direction {
+			case .right:
+				PlayerGUI.text = "Play Fast" 
+				audioPlayer.rate = 2
+			case .left:
+				PlayerGUI.text = "Play Slow" 
+				audioPlayer.rate = 0.5 
+			case .up:
+				PlayerGUI.text = "Let there be Light!" 
+				lightOn = true 
+			    updateUI()
+			case .down:
+				PlayerGUI.text = "Let there be Night!" 
+				lightOn = false 
+			    updateUI()
+			default:
+				PlayerGUI.text = "" 
+			
 		}
+		    bgView.backgroundColor = random()
 	}
-	@IBAction func SwipeLeft(_ sender: UISwipeGestureRecognizer) {
-		if sender.direction == .left {
-		print("Swipped Left")
-		audioPlayer.play(atTime: currentTime - 30.0)
-		backgroundView.backgroundColor = random()
-	    }
+	
+	func updateUI() {
+		if lightOn {
+			view.backgroundColor = .white
+			PlayerGUI.backgroundColor = .yellow
+			PlayerGUI.textColor = .purple
+		}	else {
+			view.backgroundColor = .black
+			PlayerGUI.backgroundColor = nightYellow
+			PlayerGUI.textColor = nightText
+		}
+		    setNeedsStatusBarAppearanceUpdate() 
 	}
+	
 	@IBAction func LongPressStop(_ sender: UILongPressGestureRecognizer) {
-		audioPlayer.stop();
+		audioPlayer.stop() 
+		PlayerGUI.text = "Tap To Continue Music"
 	}
 	@IBAction func TapToPlay(_ sender: UITapGestureRecognizer) {
-		audioPlayer.play();
+		audioPlayer.play() 
+		PlayerGUI.text = "Long Press To Stop"
 	}
 	@IBAction func valueChanged(_ sender: UISlider) {
-		audioPlayer.volume = sender.value;
+		audioPlayer.volume = sender.value 
 		
 	}
 	
-	
+	override var preferredStatusBarStyle: UIStatusBarStyle {
+        return lightOn ? .darkContent : .lightContent 
+    }
 
 	
 	
